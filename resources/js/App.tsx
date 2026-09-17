e612815a0090755074b2f3f337e115b4ad50d384
monte <hairs-immense4n@icloud.com>
2026-09-17 15:25:07 +0200

    add react frontend shell

---
+++ resources/js/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <main>
+            <h1>Grounded</h1>
+            <p>Asystent FAQ oparty o źródła.</p>
+        </main>
+    );
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+createRoot(document.getElementById('app')).render(
+    React.createElement(App),
+);
